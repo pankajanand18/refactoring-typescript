be762f95a052b710e81c96beb11b8a263b34ce99
Pankaj Anand <pankajanand18@gmail.com>
2021-10-16 13:14:09 -0700

    reverseArra

--- src/methods.ts
+++ src/methods.ts
@@ -1,14 +1,13 @@
 /**
- * @description Reverse an input array in place  
- * @param nums 
+ * @description Reverse an input array in place
+ * @param nums
  */
-function reverseArray(nums: number[]){
-    for(let i=0;i<nums.length/2;i++){
-
-        let positionToSwap =nums.length-1-i
+function reverseArray(nums: number[]) {
+    for (let i = 0; i < nums.length / 2; i++) {
+        let positionToSwap = nums.length - 1 - i
         let temp = nums[i]
         nums[i] = nums[positionToSwap]
-        nums[positionToSwap] = temp 
+        nums[positionToSwap] = temp
 
     }
 
